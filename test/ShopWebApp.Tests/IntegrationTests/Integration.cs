9b16577cca80a483276cf8b65c1c80ff42291a91
Walid HAIDARI <walid.haidari@soprasteria.com>
2024-07-01 12:06:15 +0200

    added proj for tests

---
+++ test/ShopWebApp.Tests/IntegrationTests/Integration.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ShopWebApp.Tests.IntegrationTests;
+
+[Trait("Category", "Integration")]
+public abstract class Integration(IConfiguration? configuration = null)
+{
+    protected IServiceProvider ServiceProvider { get; } = CreateServices(configuration).BuildServiceProvider();
+    private static ServiceCollection CreateServices(IConfiguration? configuration = null)
+    {
+        var config = configuration ?? new ConfigurationBuilder()
+            .AddUserSecrets(Assembly.GetExecutingAssembly())
+            .AddEnvironmentVariables()
+            .Build();
+        
+        var services = new ServiceCollection();
+        services.AddSingleton(config);
+        return services;
+    }
+    
+}
--- test/ShopWebApp.Tests/UnitTest1.cs
+++ test/ShopWebApp.Tests/UnitTest1.cs
@@ -1,4 +1,4 @@
-namespace TestProject1;
+namespace ShopWebApp.Tests;
 
 public class UnitTest1
 {
